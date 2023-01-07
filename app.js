ee25033c70a0e6f71b787fba697a3fa24f021f89
koh-gt <101822992+koh-gt@users.noreply.github.com>
2023-01-08 04:03:25 +0800

    Enable CORS for API
    you may face
    CORS header ‘Access-Control-Allow-Origin’ missing
    
    Enable Cross-Origin Resource Sharing so that websites can access block explorer API.
    
    fetch('http://explorer.ferritecoin.org/api/getblockcount', myInit)
                  .then((response) => response.json())
                  .then((data) => console.log(data))

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ var express = require('express')
   , request = require('request');
 
 var app = express();
+// enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // bitcoinapi
 bitcoinapi.setWalletDetails(settings.wallet);
